687e74c82519cd81445f2c8157175713db5adf80
Dorin_Suciu <dorinsuciu1999@yahoo.com>
2021-10-30 18:50:18 +0300

    Setup the project

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Converter  />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Converter from "./components/Converter.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Converter,
   },
 };
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Converter.vue
@@ -0,0 +1,7 @@
+<template>
+      <div>This is empty</div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
